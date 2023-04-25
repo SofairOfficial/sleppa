44c9c277b3efa3d2c968410ebbcea3aaeb390457
GuiBL4 <guillaume@sofair.io>
2023-04-25 08:23:04 +0200

    feat: implementation of trait ReleaseRuleHandler

--- crates/sleppa_configuration/src/lib.rs
+++ crates/sleppa_configuration/src/lib.rs
@@ -2,36 +2,40 @@
 //!
 //! Sleppa configuration parser reads the `sleppa.toml` configuration file and deserializes it.
 //!
-//! This parser reads the `sleppa.toml` configuration file and convert it to Rust structure [`Configuration`].
+//! This parser reads the `sleppa.toml` configuration file and converts it to Rust structure [Configuration].
 //! This configuration file should contain a `[release_rule]` section with the 3 release types: `major`, `minor` and `patch`.
 //! These release types are mandatory and are case sensitive. Here an exemple of such a file :
 //!
-//! ```toml
+//!```toml
 //! [release_rules]
 //! major = { format = "regex", grammar = '^(?P<type>break){1}(?P<scope>\(\S.*\S\))?:\s.*[a-z0-9]$' }
 //! minor = { format = "regex", grammar = '^(?P<type>ci|feat){1}(?P<scope>\(\S.*\S\))?:\s.*[a-z0-9]$' }
 //! patch = { format = "regex", grammar = '^(?P<type>fix|refac|test){1}(?P<scope>\(\S.*\S\))?:\s.*[a-z0-9]$' }
 //!```
 //!
-//!For each release type, user must define a `format` to read the associated `grammar`.
+//! For each release type, user must define a `format` to read the associated `grammar`.
 //!
-//!The supported format are defined by an enum [`ReleaseRuleFormat`] :
+//! The supported format are defined by an enum [ReleaseRuleFormat] :
 //! - Regex: declared by `regex`
 //! - Peg: declared by `peg`
 //!
-//! The function `try_parse_sleppatoml` will return a [`Configuration`] :
-//! - `Hashmap<ReleaseAction, ReleaseRule { ReleaseRuleFormat, String }>`
+//! The function [try_parse] returns a [Configuration] :
+//! - `Hashmap<ReleaseAction, ReleaseRuleDefinition { ReleaseRuleFormat, String }>`
+//!
+//! The trait [ReleaseRuleHandler] handles the release rule definition and verifies if a message
+//! matches a [ReleaseAction].
 //!
 
 mod error;
 
 use error::{ConfigurationError, ConfigurationResult};
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
 
-/// Enumerates available release actions
+/// Enumerates available release actions.
 #[derive(PartialEq, Debug, Serialize, Deserialize, Eq, Hash)]
 #[serde(rename_all = "lowercase")]
 pub enum ReleaseAction {
@@ -43,7 +47,7 @@ pub enum ReleaseAction {
     Patch,
 }
 
-/// Enumerates available format for a release rule
+/// Enumerates available format for a release rule.
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "lowercase")]
 pub enum ReleaseRuleFormat {
@@ -53,19 +57,20 @@ pub enum ReleaseRuleFormat {
     Peg,
 }
 
-/// `Configuration` structure
+/// The new type associated with the `release_rules` section in the `sleppa.toml`.
+pub type ReleaseRules = HashMap<ReleaseAction, ReleaseRuleDefinition>;
+
+/// This structure will be used to deserialize the toml into this Rust usable type.
 ///
-/// This structure will be used to deserialize the toml into this Rust usable
-/// type.
 /// The `release_rules` hashmap contains 3 keys : `major`, `minor` and `patch`.
-/// For every key a [`ReleaseRule`] is associated.
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Configuration<F> {
-    pub release_rules: HashMap<ReleaseAction, ReleaseRule<F>>,
+/// For every key a [ReleaseRuleDefinition] is associated.
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct Configuration {
+    pub release_rules: ReleaseRules,
 }
 
 /// Implementation of the `new` method : `Configuration::new()`
-impl Configuration<ReleaseRuleFormat> {
+impl Configuration {
     pub fn new() -> Self {
         Configuration {
             release_rules: HashMap::new(),
@@ -73,45 +78,56 @@ impl Configuration<ReleaseRuleFormat> {
     }
 }
 
-/// Implementation of the `default` method : `Configuration::default()`
-impl Default for Configuration<ReleaseRuleFormat> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-/// `ReleaseRule` structure
-///
-/// The ReleaseRule defined a grammar and the format associated to analyze it.
-/// It is generic over F to implement any convenient format.
+/// The ReleaseRuleDefinition contains a grammar and the format associated to parse it.
 #[derive(Debug, Serialize, Deserialize)]
-pub struct ReleaseRule<F> {
-    pub format: F,
+pub struct ReleaseRuleDefinition {
+    /// The format is `Regex` or `Peg`
+    pub format: ReleaseRuleFormat,
     pub grammar: String,
 }
 
-/// `ReleaseRuleValidator` trait
-///
-/// Trait to implement to to validate the [`ReleaseRule`] over a generic `format` F.
-pub trait ReleaseRuleValidator {
-    type F;
-    /// The `validate` function reads a message, compare it to a `grammar` in a `format` F
-    /// and return a [`ReleaseAction`] if found.
-    fn validate<F>(&self, message: String) -> Option<ReleaseAction>;
+/// Trait to analyze a `message` and if it matches a [ReleaseRules].
+pub trait ReleaseRuleHandler {
+    /// Reads a message, compares it to a `grammar` in a `format` (Regex or Peg)
+    /// and returns a boolean. True if there is a match , false otherwise.
+    fn handle(&self, message: &str) -> ConfigurationResult<bool>;
 }
 
-/// try_parser_sleppatoml function
+/// Implementation of the trait [ReleaseRuleHandler] for [ReleaseRuleDefinition].
+impl ReleaseRuleHandler for ReleaseRuleDefinition {
+    fn handle(&self, message: &str) -> ConfigurationResult<bool> {
+        match &self.format {
+            ReleaseRuleFormat::Regex => {
+                let re = match Regex::new(self.grammar.as_str()) {
+                    Ok(re) => re,
+                    Err(err) => {
+                        return Err(ConfigurationError::RegexError(err));
+                    }
+                };
+                let captured = match re.captures(message) {
+                    Some(_cap) => true,
+                    None => false,
+                };
+                Ok(captured)
+            }
+            ReleaseRuleFormat::Peg => {
+                todo!()
+            }
+        }
+    }
+}
+
+/// Tries parsing the `sleppa.toml` configuration file and returns a [Configuration] or
+/// a [ConfigurationError].
 ///
-/// Try to parse the `sleppa.toml` configuration file and return a [`Configuration`] structure or
-/// an `Error`
-pub fn try_parse_sleppatoml(path: &Path) -> ConfigurationResult<Configuration<ReleaseRuleFormat>> {
-    let reading = fs::read_to_string(path)?;
+/// The `path` is the path to the configuration file `sleppa.toml`.
+/// The parsing returns a [ConfigurationError] if a [ReleaseAction] is missing or if the `format` is not recognized.
+pub fn try_parse(path: &Path) -> ConfigurationResult<Configuration> {
+    let content = fs::read_to_string(path)?;
 
-    let config: Configuration<ReleaseRuleFormat> = toml::from_str(&reading)?;
+    let config: Configuration = toml::from_str(&content)?;
 
-    println!("{:?}", config);
-
-    // Verify the `sleppa.toml` contains the exact 3 [`ReleaseAction`] : `Major`, `Minor` and `Patch`
+    // Verify that `sleppa.toml` contains a release rule for each release action types.
     if config.release_rules.get(&ReleaseAction::Major).is_none() {
         return Err(ConfigurationError::IncorrectReleaseAction(
             "major is missing".to_string(),
--- crates/sleppa_configuration/src/tests.rs
+++ crates/sleppa_configuration/src/tests.rs
@@ -1,6 +1,6 @@
 //! # Unit tests
 //!
-//! This module contains all the unit tests of the sleppa_configuration crate.
+//! This testing module implements the unit tests for testing the configuration processing routines.
 
 use super::*;
 use std::fs::File;
@@ -11,37 +11,37 @@ use tempfile::tempdir;
 fn test_ok_parser_sleppatoml() {
     let tmp_dir = tempdir().unwrap();
     let file_path = tmp_dir.path().join("sleppa.toml");
-    let mut sleppatoml = File::create(&file_path).unwrap();
+    let mut file = File::create(&file_path).unwrap();
 
-    writeln!(&mut sleppatoml, "[release_rules]").unwrap();
+    writeln!(&mut file, "[release_rules]").unwrap();
     writeln!(
-        &mut sleppatoml,
+        &mut file,
         r#"major = {{ format = "regex" , grammar = '^(?P<type>break){{1}}(?P<scope>\(\S.*\S\))?:\s.*[a-z0-9]$' }}"#
     )
     .unwrap();
     writeln!(
-        &mut sleppatoml,
+        &mut file,
         r#"minor = {{ format = "regex" , grammar = '^(?P<type>feat|refac){{1}}(?P<scope>\(\S.*\S\))?:\s.*[a-z0-9]$' }}"#
     )
     .unwrap();
     writeln!(
-        &mut sleppatoml,
+        &mut file,
         r#"patch = {{ format = "regex" , grammar = '^(?P<type>fix){{1}}(?P<scope>\(\S.*\S\))?:\s.*[a-z0-9]$' }}"#
     )
     .unwrap();
 
-    let config = try_parse_sleppatoml(&file_path).expect("Failed to parse toml");
+    let config = try_parse(&file_path).expect("Failed to parse toml");
     assert_eq!(
         config.release_rules[&ReleaseAction::Major].format,
         ReleaseRuleFormat::Regex
     );
 
-    let config = try_parse_sleppatoml(&file_path).expect("Failed to parse toml");
+    let config = try_parse(&file_path).expect("Failed to parse toml");
     assert_eq!(
         config.release_rules[&ReleaseAction::Minor].format,
         ReleaseRuleFormat::Regex
     );
-    let config = try_parse_sleppatoml(&file_path).expect("Failed to parse toml");
+    let config = try_parse(&file_path).expect("Failed to parse toml");
     assert_eq!(
         config.release_rules[&ReleaseAction::Patch].format,
         ReleaseRuleFormat::Regex
@@ -64,31 +64,47 @@ fn test_ok_parser_sleppatoml() {
 }
 
 #[test]
+#[should_panic(expected = "unknown variant `rege`, expected `regex` or `peg`")]
+fn test_fail_wrong_format() {
+    let tmp_dir = tempdir().unwrap();
+    let file_path = tmp_dir.path().join("sleppa.toml");
+    let mut file = File::create(&file_path).unwrap();
+
+    writeln!(&mut file, "[release_rules]").unwrap();
+    writeln!(
+        &mut file,
+        r#"major = {{ format = "rege" , grammar = '^(?P<type>break){{1}}(?P<scope>\(\S.*\S\))?:\s.*[a-z0-9]$' }}"#
+    )
+    .unwrap();
+    let _config = try_parse(&file_path).expect("Failed to parse toml");
+}
+
+#[test]
 #[should_panic(expected = "unknown variant `Major`, expected one of `major`, `minor`, `patch`")]
 fn test_fail_case_sensitive_sleppatoml() {
     let tmp_dir = tempdir().unwrap();
     let file_path = tmp_dir.path().join("sleppa.toml");
-    let mut sleppatoml = File::create(&file_path).unwrap();
+    let mut file = File::create(&file_path).unwrap();
 
     // The `sleppa.toml` file is case sensitive over "major", "minor" and "patch".
-    writeln!(&mut sleppatoml, "[release_rules]").unwrap();
+    writeln!(&mut file, "[release_rules]").unwrap();
     writeln!(
-        &mut sleppatoml,
+        &mut file,
         r#"Major = {{ format = "regex" , grammar = '^(break){{1}}(\(\S.*\S\))?:\s.*[a-z0-9]$' }}"#
     )
     .unwrap();
     writeln!(
-        &mut sleppatoml,
+        &mut file,
         r#"minor = {{ format = "regex" , grammar = '^(feat|refac){{1}}(\(\S.*\S\))?:\s.*[a-z0-9]$' }}"#
     )
     .unwrap();
     writeln!(
-        &mut sleppatoml,
+        &mut file,
         r#"patch = {{ format = "regex" , grammar = '^(?P<type>fix){{1}}(\(\S.*\S\))?:\s.*[a-z0-9]$' }}"#
     )
     .unwrap();
 
-    let _config = try_parse_sleppatoml(&file_path).expect("Failed to parse toml");
+    let _config = try_parse(&file_path).expect("Failed to parse toml");
 }
 
 #[test]
@@ -96,21 +112,21 @@ fn test_fail_case_sensitive_sleppatoml() {
 fn test_fail_missing_release_sleppatoml() {
     let tmp_dir = tempdir().unwrap();
     let file_path = tmp_dir.path().join("sleppa.toml");
-    let mut sleppatoml = File::create(&file_path).unwrap();
+    let mut file = File::create(&file_path).unwrap();
 
-    writeln!(&mut sleppatoml, "[release_rules]").unwrap();
+    writeln!(&mut file, "[release_rules]").unwrap();
     writeln!(
-        &mut sleppatoml,
+        &mut file,
         r#"major = {{ format = "regex" , grammar = '^(break){{1}}(\(\S.*\S\))?:\s.*[a-z0-9]$' }}"#
     )
     .unwrap();
     writeln!(
-        &mut sleppatoml,
+        &mut file,
         r#"minor = {{ format = "regex" , grammar = '^(feat|refac){{1}}(\(\S.*\S\))?:\s.*[a-z0-9]$' }}"#
     )
     .unwrap();
 
-    let _config = try_parse_sleppatoml(&file_path).expect("Failed to parse toml");
+    let _config = try_parse(&file_path).expect("Failed to parse toml");
 }
 
 #[test]
@@ -118,23 +134,46 @@ fn test_fail_missing_release_sleppatoml() {
 fn test_fail_missing_field_sleppatoml() {
     let tmp_dir = tempdir().unwrap();
     let file_path = tmp_dir.path().join("sleppa.toml");
-    let mut sleppatoml = File::create(&file_path).unwrap();
+    let mut file = File::create(&file_path).unwrap();
 
     writeln!(
-        &mut sleppatoml,
+        &mut file,
         r#"major = {{ format = "regex" , grammar = '^(break){{1}}(\(\S.*\S\))?:\s.*[a-z0-9]$' }}"#
     )
     .unwrap();
     writeln!(
-        &mut sleppatoml,
+        &mut file,
         r#"minor = {{ format = "regex" , grammar = '^(feat|refac){{1}}(\(\S.*\S\))?:\s.*[a-z0-9]$' }}"#
     )
     .unwrap();
     writeln!(
-        &mut sleppatoml,
+        &mut file,
         r#"patch = {{ format = "regex" , grammar = '^(?P<type>fix){{1}}(\(\S.*\S\))?:\s.*[a-z0-9]$' }}"#
     )
     .unwrap();
 
-    let _config = try_parse_sleppatoml(&file_path).expect("Failed to parse toml");
+    let _config = try_parse(&file_path).expect("Failed to parse toml");
+}
+
+#[test]
+fn test_ok_trait_implementation_regex() {
+    let release_rule_def = ReleaseRuleDefinition {
+        format: ReleaseRuleFormat::Regex,
+        grammar: r"^(?P<type>feat|ci){1}(?P<scope>\(\S.*\S\))?:\s.*[a-z0-9]$".to_string(),
+    };
+
+    let msg0 = "feat: add a function";
+    let msg1 = "feat(sync): add a function";
+    let msg2 = "ci: add a workflow";
+    let msg3 = "break(sync): add a function";
+    let msg4 = "feat (sync): add a function";
+    let msg5 = "feat add a function";
+
+    assert!(release_rule_def.handle(msg0).unwrap());
+    assert!(release_rule_def.handle(msg1).unwrap());
+    assert!(release_rule_def.handle(msg2).unwrap());
+
+    assert!(!release_rule_def.handle(msg3).unwrap());
+    assert!(!release_rule_def.handle(msg4).unwrap());
+    assert!(!release_rule_def.handle(msg5).unwrap());
 }
